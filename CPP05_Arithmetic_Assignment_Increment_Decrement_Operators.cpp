6f66adcb37d3c7340aae144542cb5307b8a435c0
codewithabhi008 <codewithabhi008@gmail.com>
2026-08-24 23:10:38 +0530

    addition to multiplication

---
+++ CPP05_Arithmetic_Assignment_Increment_Decrement_Operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a=10;
+    int b=5;
+
+    // This all call Arithmetic .
+    cout << a + b << endl; // Addition
+    cout << a - b << endl; // Subtraction
+    cout << a / b << endl; // Division
+    cout << a * b << endl; // Multiplication
+
+    // This is call Decrement operator.
+    cout << ++a <<endl; // Pre - Increment
+    cout << --a <<endl; // Pre - decrement
+    cout << a++ <<endl; // Post - Increment
+    cout << a-- <<endl; // Post - decrement
+
+    return 0;
+}
