e65d8e34dfe18a38f2a54c5d4b8aafd63cd5e186
codewithabhi008 <codewithabhi008@gmail.com>
2026-08-24 22:56:58 +0530

    new

---
+++ CPP04_Input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    // cin - console input
+    int a;
+    cin >> a;
+    cout << "Variabler name a = " << a << endl;
+
+    string name,surname;
+
+    cout << "Enter your name : ";
+    cin >> name;
+
+    cout << "Enter your surname : ";
+    cin >> surname;
+
+
+    return 0;
+}
