29ee4eb8c6a99af8d2ede48ce7dc693716ae98f0
codewithabhi008 <codewithabhi008@gmail.com>
2026-08-26 19:25:14 +0530

    commit

--- CPP05_Arithmetic_Assignment_Increment_Decrement_Operators.cpp
+++ CPP05_Arithmetic_Assignment_Increment_Decrement_Operators.cpp
@@ -14,7 +14,7 @@ int main(){
     cout << a * b << endl; // Multiplication
 
     // This is call Decrement operator.
-    cout << ++a <<endl; // Pre - Increment
+    cout << ++a <<endl; // Pre - Increment i++=i=i+1 i--=i=i-1
     cout << --a <<endl; // Pre - decrement
     cout << a++ <<endl; // Post - Increment
     cout << a-- <<endl; // Post - decrement
